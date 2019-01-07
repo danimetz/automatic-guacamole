7a24cc365c4dc617d7e2ef4296fcd2e9881faa97
Danielle Metzner <danimetzner@gmail.com>
2019-01-07 13:44:24 -0800

    Updated the DB to postgresql

--- Jukebox_API/settings.py
+++ Jukebox_API/settings.py
@@ -11,8 +11,35 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-load_dotenv()
+import environ
+
+env = environ.Env(
+# set casting, default value
+DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+# False if not in os.environ
+DEBUG = env('DEBUG')
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
+DATABASES = {
+# read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
+'default': env.db(),
+# read os.environ['SQLITE_URL']
+'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
+}
+
+CACHES = {
+# read os.environ['CACHE_URL'] and raises ImproperlyConfigured exception if not found
+'default': env.cache(),
+# read os.environ['REDIS_URL']
+'redis': env.cache('REDIS_URL')
+}
 
 
 
@@ -87,8 +114,12 @@ WSGI_APPLICATION = 'Jukebox_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capstoneDB',
+        'USER': 'danimetz',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
